a1dbc1209bb84e40a023cb7b1791b2cd9853871e
Tom Anderson <tom@ando.gq>
2021-06-17 21:31:50 +1000

    Add some comments

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,32 @@ use glutin::event_loop::{ControlFlow, EventLoop};
 use glutin::window::WindowBuilder;
 use glutin::ContextBuilder;
 
-extern crate gl;
-
 use std::ffi::CStr;
 
 fn main() {
+    // Create the window and event loop
     let el = EventLoop::new();
     let wb = WindowBuilder::new().with_title("Hello");
 
+    // Load the context
     let context = ContextBuilder::new().build_windowed(wb, &el).unwrap();
     let context = unsafe { context.make_current().unwrap() };
 
-    println!("Pixel format of the window's GL context: {:?}", context.get_pixel_format());
-
+    // Load OpenGL function wrapper
     gl::load_with(|s| context.get_proc_address(s));
 
+    // Get OpenGL version
     unsafe {
         let data = gl::GetString(gl::VERSION);
         println!("{}", String::from_utf8(CStr::from_ptr(data as *const i8).to_bytes().to_vec()).unwrap());
     };
 
+    // Set the clear color
     unsafe { gl::ClearColor(0.0, 0.0, 0.0, 1.0) };
 
+    // Run the event loop
     el.run(move |event, _, control_flow| {
-
+        // Check what type of event has been called
         match event {
             Event::LoopDestroyed => return,
             Event::WindowEvent { event, .. } => match event {
